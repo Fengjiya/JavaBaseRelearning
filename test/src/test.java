a9d31f626df6ca12ef73e0ff70bca123bb426816
Fengjiya <fengjiya@gmail.com>
2014-09-26 14:50:33 +0800

    Nothing
    .

--- test/src/test.java
+++ test/src/test.java
@@ -3,10 +3,10 @@ import java.util.Random;
 
 public class test
 {
-	public static final long NUMBER = 100000000000L;
+	public static final long NUMBER = 100000L;
 	public static void main(String[] args)
 	{
-		long[] idArray = new long[NUMBER];
+		long[] idArray = new long[(int)NUMBER];
 		for( int i = 0; i <NUMBER; i++ )
 		{
 			
