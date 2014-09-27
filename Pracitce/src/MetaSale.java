97b4fef6ac61e07faed729cb22afa36b42b0d081
fengjiya <1029877436>
2014-09-27 15:53:45 +0800

    Complete  the SalesFigures.
    But failed int the DB connection test.

---
+++ Pracitce/src/MetaSale.java
@@ -0,0 +1,26 @@
+
+public class MetaSale
+{
+	private String month;
+	private double sale;
+	
+	public MetaSale( String month, double sale )
+	{
+		this.month = month;
+		this.sale = sale;
+	}
+	
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public double getSale() {
+		return sale;
+	}
+	public void setSale(double sale) {
+		this.sale = sale;
+	}
+
+}
