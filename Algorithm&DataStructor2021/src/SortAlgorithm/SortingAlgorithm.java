8982dbd91a0aa5503385c392521aef7d6fffb0ee
Fengjiya <fengjiya@gmail.com>
2021-08-10 23:27:02 +0800

    【Add】2021年8月10日：1、增加插入排序；2、基本性能比较：选择排序>插入排序>冒泡排序

--- Algorithm&DataStructor2021/src/SortAlgorithm/SortingAlgorithm.java
+++ Algorithm&DataStructor2021/src/SortAlgorithm/SortingAlgorithm.java
@@ -4,21 +4,21 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class SortingAlgorithm {
-    static final int arrayLength = 10000;
-    static final int checkTimes = 100;
+    static final int ARRAY_LENGTH = 100000;
+    static final int CHECK_TIMES = 10;
 
     public int[] randomArrayGenerator() {
         Random random = new Random();
 
-        int[] array = new int[arrayLength];
+        int[] array = new int[ARRAY_LENGTH];
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(arrayLength);
+            array[i] = random.nextInt(ARRAY_LENGTH);
         }
         return array;
     }
 
-    public int[] SelectSorting(int[] noneOrderArray) {
+    public int[] selectSorting(int[] noneOrderArray) {
         /*
          * 最简单，但是最不适用的排序算法，有优化空间
          *随机生产数据器、对数器
@@ -57,7 +57,7 @@ public class SortingAlgorithm {
         }
     }
 
-    public int[] BubbleSorting(int[] noneOrderArray) {
+    public int[] bubbleSorting(int[] noneOrderArray) {
         /*
          * 时间复杂度：O(n²)
          * 空间复杂度：O(1)
@@ -79,19 +79,39 @@ public class SortingAlgorithm {
         return noneOrderArray;
     }
 
-    public void check() {
-        System.out.println("长度为" + arrayLength + "的数组，排序" + checkTimes + "次，开始排序...");
+    public int[] insertSorting(int[] noneOrderArray) {
+        /*
+         * 插入排序，对于基本有序的数组，速度快，而且是稳定排序
+         * */
+        if (noneOrderArray == null || noneOrderArray.length == 0) {
+            return null;
+        } else if (noneOrderArray.length == 1) {
+            return noneOrderArray;
+        }
+
+        for (int i = 1; i < noneOrderArray.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (noneOrderArray[j] > noneOrderArray[j - 1]) {
+                    swapArrayValue(noneOrderArray, j, j - 1);
+                }
+            }
+
+        }
+
+        return noneOrderArray;
+    }
+
+    public void sortingCheck() {
+        System.out.println("长度为" + ARRAY_LENGTH + "的数组，排序" + CHECK_TIMES + "次，开始排序...");
         long start = System.currentTimeMillis();
-        for (int i = 0; i < checkTimes; i++) {
+        for (int i = 0; i < CHECK_TIMES; i++) {
             int[] arr1 = randomArrayGenerator();
             int[] arr2 = Arrays.copyOf(arr1, arr1.length);
 
-            arr1 = SelectSorting(arr1);
+            arr1 = bubbleSorting(arr1);
             Arrays.sort(arr2);
 
             Arrays.equals(arr1, arr2);
-
-//            System.out.println("第" + (i + 1) + "次循环：" + (!Arrays.equals(arr1, arr2) ? "排序不相等" : "排序相等"));
         }
 
         System.out.println("排序结束，合计耗时：" + (System.currentTimeMillis() - start) / 1000 + "S");
@@ -101,7 +121,7 @@ public class SortingAlgorithm {
     public static void main(String[] args) {
         int[] array = {3, 5, 2, 8, 1};
         SortingAlgorithm sortingAlgorithm = new SortingAlgorithm();
-        sortingAlgorithm.check();
+        sortingAlgorithm.sortingCheck();
 //        System.out.println(Arrays.toString(sortingAlgorithm.BubbleSorting(array)));
     }
 
