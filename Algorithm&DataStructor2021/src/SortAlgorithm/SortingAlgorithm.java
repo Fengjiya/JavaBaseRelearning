5ec5952db1a488629593152a0c7e3ae2b9c3d043
Fengjiya <fengjiya@gmail.com>
2021-08-10 23:42:39 +0800

    【Update】2021年8月10日：优化插入排序

--- Algorithm&DataStructor2021/src/SortAlgorithm/SortingAlgorithm.java
+++ Algorithm&DataStructor2021/src/SortAlgorithm/SortingAlgorithm.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class SortingAlgorithm {
-    static final int ARRAY_LENGTH = 100000;
-    static final int CHECK_TIMES = 10;
+    static final int ARRAY_LENGTH = 10000;
+    static final int CHECK_TIMES = 100;
 
     public int[] randomArrayGenerator() {
         Random random = new Random();
@@ -18,15 +18,16 @@ public class SortingAlgorithm {
         return array;
     }
 
-    public int[] selectSorting(int[] noneOrderArray) {
+    public int selectSorting(int[] noneOrderArray, int counter) {
         /*
          * 最简单，但是最不适用的排序算法，有优化空间
          *随机生产数据器、对数器
          * */
+        System.out.println("这是选择排序");
         if (noneOrderArray == null || noneOrderArray.length == 0) {
-            return null;
+            return counter;
         } else if (noneOrderArray.length == 1) {
-            return noneOrderArray;
+            return counter;
         }
 
         int minIndex;
@@ -38,6 +39,7 @@ public class SortingAlgorithm {
                 if (noneOrderArray[innerIndex] < noneOrderArray[minIndex]) {
                     minIndex = innerIndex;
                 }
+                counter++;
 
             }
 
@@ -45,7 +47,7 @@ public class SortingAlgorithm {
 //            System.out.println("第" + (outerIndex + 1) + "次循环：" + Arrays.toString(noneOrderArray));
         }
 
-        return noneOrderArray;
+        return counter;
     }
 
     private void swapArrayValue(int[] noneOrderArray, int srcIndex, int destIndex) {
@@ -57,15 +59,16 @@ public class SortingAlgorithm {
         }
     }
 
-    public int[] bubbleSorting(int[] noneOrderArray) {
+    public int bubbleSorting(int[] noneOrderArray, int counter) {
         /*
          * 时间复杂度：O(n²)
          * 空间复杂度：O(1)
          * */
+        System.out.println("这是冒泡排序");
         if (noneOrderArray == null || noneOrderArray.length == 0) {
-            return null;
+            return counter;
         } else if (noneOrderArray.length == 1) {
-            return noneOrderArray;
+            return counter;
         }
 
         for (int i = 0; i < noneOrderArray.length - 1; i++) {
@@ -73,48 +76,56 @@ public class SortingAlgorithm {
                 if (noneOrderArray[j] > noneOrderArray[j + 1]) {
                     swapArrayValue(noneOrderArray, j, j + 1);
                 }
+                counter++;
             }
         }
 
-        return noneOrderArray;
+        return counter;
     }
 
-    public int[] insertSorting(int[] noneOrderArray) {
+    public int insertSorting(int[] noneOrderArray, int counter) {
         /*
          * 插入排序，对于基本有序的数组，速度快，而且是稳定排序
          * */
+        System.out.println("这是插入排序");
         if (noneOrderArray == null || noneOrderArray.length == 0) {
-            return null;
+            return counter;
         } else if (noneOrderArray.length == 1) {
-            return noneOrderArray;
+            return counter;
         }
 
         for (int i = 1; i < noneOrderArray.length; i++) {
             for (int j = i; j > 0; j--) {
-                if (noneOrderArray[j] > noneOrderArray[j - 1]) {
+                if (noneOrderArray[j] < noneOrderArray[j - 1]) {
                     swapArrayValue(noneOrderArray, j, j - 1);
+                    counter++;
+                } else {
+                    break;
                 }
             }
 
+
         }
 
-        return noneOrderArray;
+        return counter;
     }
 
     public void sortingCheck() {
         System.out.println("长度为" + ARRAY_LENGTH + "的数组，排序" + CHECK_TIMES + "次，开始排序...");
         long start = System.currentTimeMillis();
+        int counter = 0;
         for (int i = 0; i < CHECK_TIMES; i++) {
             int[] arr1 = randomArrayGenerator();
             int[] arr2 = Arrays.copyOf(arr1, arr1.length);
 
-            arr1 = bubbleSorting(arr1);
+            counter = bubbleSorting(arr1, counter);
             Arrays.sort(arr2);
 
             Arrays.equals(arr1, arr2);
         }
 
         System.out.println("排序结束，合计耗时：" + (System.currentTimeMillis() - start) / 1000 + "S");
+        System.out.println("合计循环判断次数：" + counter);
 
     }
 
