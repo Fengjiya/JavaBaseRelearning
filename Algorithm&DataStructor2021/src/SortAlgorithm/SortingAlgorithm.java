6c09a9742d30a758c2333ffe831445d3e13c4388
Fengjiya <fengjiya@gmail.com>
2021-08-12 00:24:25 +0800

    【Add】2021年8月12日：增加归并排序算法实现

--- Algorithm&DataStructor2021/src/SortAlgorithm/SortingAlgorithm.java
+++ Algorithm&DataStructor2021/src/SortAlgorithm/SortingAlgorithm.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class SortingAlgorithm {
-    static final int ARRAY_LENGTH = 10000;
-    static final int CHECK_TIMES = 100;
+    static final int ARRAY_LENGTH = 100000;
+    static final int CHECK_TIMES = 10;
 
     public int[] randomArrayGenerator() {
         Random random = new Random();
@@ -18,16 +18,16 @@ public class SortingAlgorithm {
         return array;
     }
 
-    public int selectSorting(int[] noneOrderArray, int counter) {
+    public int[] selectSorting(int[] noneOrderArray) {
         /*
          * 最简单，但是最不适用的排序算法，有优化空间
          *随机生产数据器、对数器
          * */
         System.out.println("这是选择排序");
         if (noneOrderArray == null || noneOrderArray.length == 0) {
-            return counter;
+            return noneOrderArray;
         } else if (noneOrderArray.length == 1) {
-            return counter;
+            return noneOrderArray;
         }
 
         int minIndex;
@@ -39,7 +39,6 @@ public class SortingAlgorithm {
                 if (noneOrderArray[innerIndex] < noneOrderArray[minIndex]) {
                     minIndex = innerIndex;
                 }
-                counter++;
 
             }
 
@@ -47,28 +46,25 @@ public class SortingAlgorithm {
 //            System.out.println("第" + (outerIndex + 1) + "次循环：" + Arrays.toString(noneOrderArray));
         }
 
-        return counter;
+        return noneOrderArray;
     }
 
     private void swapArrayValue(int[] noneOrderArray, int srcIndex, int destIndex) {
-        int cacheValue;
-        if (srcIndex != destIndex) {
-            cacheValue = noneOrderArray[destIndex];
-            noneOrderArray[destIndex] = noneOrderArray[srcIndex];
-            noneOrderArray[srcIndex] = cacheValue;
-        }
+        int cacheValue = noneOrderArray[destIndex];
+        noneOrderArray[destIndex] = noneOrderArray[srcIndex];
+        noneOrderArray[srcIndex] = cacheValue;
     }
 
-    public int bubbleSorting(int[] noneOrderArray, int counter) {
+    public int[] bubbleSorting(int[] noneOrderArray) {
         /*
          * 时间复杂度：O(n²)
          * 空间复杂度：O(1)
          * */
         System.out.println("这是冒泡排序");
         if (noneOrderArray == null || noneOrderArray.length == 0) {
-            return counter;
+            return noneOrderArray;
         } else if (noneOrderArray.length == 1) {
-            return counter;
+            return noneOrderArray;
         }
 
         for (int i = 0; i < noneOrderArray.length - 1; i++) {
@@ -76,29 +72,27 @@ public class SortingAlgorithm {
                 if (noneOrderArray[j] > noneOrderArray[j + 1]) {
                     swapArrayValue(noneOrderArray, j, j + 1);
                 }
-                counter++;
             }
         }
 
-        return counter;
+        return noneOrderArray;
     }
 
-    public int insertSorting(int[] noneOrderArray, int counter) {
+    public int[] insertSorting(int[] noneOrderArray) {
         /*
          * 插入排序，对于基本有序的数组，速度快，而且是稳定排序
          * */
         System.out.println("这是插入排序");
         if (noneOrderArray == null || noneOrderArray.length == 0) {
-            return counter;
+            return noneOrderArray;
         } else if (noneOrderArray.length == 1) {
-            return counter;
+            return noneOrderArray;
         }
 
         for (int i = 1; i < noneOrderArray.length; i++) {
             for (int j = i; j > 0; j--) {
                 if (noneOrderArray[j] < noneOrderArray[j - 1]) {
                     swapArrayValue(noneOrderArray, j, j - 1);
-                    counter++;
                 } else {
                     break;
                 }
@@ -107,7 +101,54 @@ public class SortingAlgorithm {
 
         }
 
-        return counter;
+        return noneOrderArray;
+    }
+
+    public int[] mergeSorting(int[] noneOrderArray) {
+//        System.out.println("这是归并排序");
+        if (noneOrderArray == null || noneOrderArray.length <= 1) {
+            return noneOrderArray;
+        } else {
+            int middle = noneOrderArray.length / 2;
+            int[] leftArray = Arrays.copyOfRange(noneOrderArray, 0, middle);
+            leftArray = mergeSorting(leftArray);
+
+            int[] rightArray = Arrays.copyOfRange(noneOrderArray, middle, noneOrderArray.length);
+            rightArray = mergeSorting(rightArray);
+
+            int[] tmpArray = new int[noneOrderArray.length];
+
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            for (; i < leftArray.length & j < rightArray.length; ) {
+                if (leftArray[i] <= rightArray[j]) {
+                    tmpArray[k] = leftArray[i];
+                    i++;
+                } else {
+                    tmpArray[k] = rightArray[j];
+                    j++;
+                }
+                k++;
+            }
+
+            if (i == leftArray.length && j < rightArray.length) {
+                for (; j < rightArray.length; j++, k++) {
+                    tmpArray[k] = rightArray[j];
+                }
+            } else if (j == rightArray.length && i < leftArray.length) {
+                for (; i < leftArray.length; i++, k++) {
+                    tmpArray[k] = leftArray[i];
+                }
+            }
+
+            noneOrderArray = tmpArray;
+            return noneOrderArray;
+
+        }
+
+
     }
 
     public void sortingCheck() {
@@ -118,7 +159,7 @@ public class SortingAlgorithm {
             int[] arr1 = randomArrayGenerator();
             int[] arr2 = Arrays.copyOf(arr1, arr1.length);
 
-            counter = bubbleSorting(arr1, counter);
+            arr1 = mergeSorting(arr1);
             Arrays.sort(arr2);
 
             Arrays.equals(arr1, arr2);
@@ -130,10 +171,12 @@ public class SortingAlgorithm {
     }
 
     public static void main(String[] args) {
-        int[] array = {3, 5, 2, 8, 1};
+        int[] array = {3, 2, 1};
         SortingAlgorithm sortingAlgorithm = new SortingAlgorithm();
         sortingAlgorithm.sortingCheck();
-//        System.out.println(Arrays.toString(sortingAlgorithm.BubbleSorting(array)));
+//        System.out.println(Arrays.toString(sortingAlgorithm.mergeSorting(array)));
+//        System.out.println(99 / 2);
+//        System.out.println(99 % 2);
     }
 
 }
